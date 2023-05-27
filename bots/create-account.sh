a68e50a3ca78830edff863c24fd578434ec78a0b
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-05-27 15:30:40 +0200

    Added sh script to create a new user

---
+++ bots/create-account.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Creates a new account without having to confirm the email
+# The generated password is printed at the end of the script
+read -p 'Account name (tag, not display name): ' username
+read -p 'E-Mail: ' email
+cd /home/mastodon/live
+RAILS_ENV=production bin/tootctl accounts create "$username" --email "$email" --confirmed

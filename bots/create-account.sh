0316e68dc074a58ddfc82935399c88a6365a4f43
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-05-27 16:58:52 +0200

    Changed user to run ruby

--- bots/create-account.sh
+++ bots/create-account.sh
@@ -4,5 +4,7 @@
 # The generated password is printed at the end of the script
 read -p 'Account name (tag, not display name): ' username
 read -p 'E-Mail: ' email
+sudo -iu mastodon
 cd /home/mastodon/live
 RAILS_ENV=production bin/tootctl accounts create "$username" --email "$email" --confirmed
+exit

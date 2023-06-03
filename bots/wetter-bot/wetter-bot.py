08d0cb1fc503427365227ebb9da3a61a945d98db
EricO <spirou99@gmx.de>
2023-06-03 10:54:44 +0200

    added wetter-bot

---
+++ bots/wetter-bot/wetter-bot.py
@@ -0,0 +1,33 @@
+# from mastodon import Mastodon
+import datetime
+import json
+import urllib.request
+
+api_url = "https://wttr.in/duesseldorf_mplang=de.png"
+
+
+
+
+#download image
+def get_Image(api_url, filename):
+    urllib.request.urlretrieve(api_url, filename);
+
+
+
+
+
+if __name__ == "__main__":
+	# Setup mastodon connection:
+	mastodon = Mastodon(
+    	# access_token = 'token.secret',
+   		# api_base_url = 'https://botsin.space/'
+	)
+        
+	get_Image(api_url, "weather.png")
+
+	media_dict = mastodon.media_post("weather.png")
+	print(media_dict["id"])
+
+
+	status_dict = mastodon.status_post("Wetterbericht. (API: https://github.com/chubin/wttr.in) ", media_ids=media_dict);
+	print(status_dict)

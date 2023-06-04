f4f69e0476d8c6dad154e22c6948cc56caf14899
EricO <spirou99@gmx.de>
2023-06-04 12:41:20 +0200

    added access_token to wetter-bot

--- bots/wetter-bot/wetter-bot.py
+++ bots/wetter-bot/wetter-bot.py
@@ -1,6 +1,6 @@
-# from mastodon import Mastodon
+from mastodon import Mastodon
 import datetime
-import json
+import json   
 import urllib.request
 
 api_url = "https://wttr.in/duesseldorf_mplang=de.png"
@@ -19,10 +19,10 @@ def get_Image(api_url, filename):
 if __name__ == "__main__":
 	# Setup mastodon connection:
 	mastodon = Mastodon(
-    	# access_token = 'token.secret',
-   		# api_base_url = 'https://botsin.space/'
+    	access_token = 'sJYGYFq7-cDBW9p06QM_9UuPTAnpAQQlG_zLnZWCyiA',
+   		api_base_url = 'https://mastodon.hosting.medien.hs-duesseldorf/'
 	)
-        
+
 	get_Image(api_url, "weather.png")
 
 	media_dict = mastodon.media_post("weather.png")

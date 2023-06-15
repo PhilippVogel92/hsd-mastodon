ecfe0a57594bb04073d2a65749cd375a48bb418d
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-06-15 14:07:49 +0200

    add routes for spacy

--- recommender_api/routes/recommender_route.py
+++ recommender_api/routes/recommender_route.py
@@ -1,17 +1,31 @@
 from flask import Blueprint, jsonify, request
-from recommender_api.utils.tfidf import recommend_with_tfidf, recommend_with_tfidf_for_account
-recommender_route = Blueprint('recommender_route', __name__)
 
-@recommender_route.route('/recommend-tfidf', methods=['POST'])
-#@cross_origin()
-def get_recommendations():
-    user_input = request.get_json()
-    recommendations = recommend_with_tfidf(user_input['sentence'], user_input['number_of_recommendations'])
-    return jsonify(recommendations)
+from recommender_api.utils.tfidf_demo import (
+    recommend_with_tfidf_for_account,
+)
 
-@recommender_route.route('/recommend-tfidf/account', methods=['POST'])
-#@cross_origin()
+from recommender_api.utils.ranking_system import get_recommendations_with_ranking_system
+
+recommender_route = Blueprint("recommender_route", __name__)
+
+
+@recommender_route.route("/recommend-tfidf/account", methods=["POST"])
+# @cross_origin()
 def get_account_recommendations():
     user_input = request.get_json()
-    recommendations = recommend_with_tfidf_for_account(user_input['account_id'], user_input['number_of_recommendations'])
+    recommendations = recommend_with_tfidf_for_account(
+        user_input["account_id"], user_input["number_of_recommendations"]
+    )
+    return jsonify(recommendations)
+
+
+@recommender_route.route("/recommend-ranking-system", methods=["POST"])
+# @cross_origin()
+def get_account_recommendations_with_ranking_system():
+    user_input = request.get_json()
+    recommendations = get_recommendations_with_ranking_system(
+        user_input["account_id"],
+        user_input["number_of_recommendations"],
+        user_input["similarity_concept"],
+    )
     return jsonify(recommendations)

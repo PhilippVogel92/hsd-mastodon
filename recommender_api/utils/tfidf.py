ef1baaa67d70d7ba889954f601206c0766440d40
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-06-15 14:09:12 +0200

    change tfidf

--- recommender_api/utils/tfidf.py
+++ recommender_api/utils/tfidf.py
@@ -1,79 +1,48 @@
 import pandas as pd
-import os
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
-import json
-import requests
-from functools import reduce
 from .preprocessing import sentence_preprocessing
+from recommender_api.model.mastodon_data import get_account_toots, get_followed_accounts
 
-def load_test_data(filename, path):
-    data = pd.read_csv(os.path.join(path, filename), sep=";")
-    
-    #select only english and german toots
-    mask_language = (data["language"] == "en") | (data["language"] == "de")
-    data = data[mask_language]
-    
-    return data
-
-
-def recommend_with_tfidf(sentence, number_of_recommendations=10):
-    
-    test_toot_df = load_test_data("mastodon.social_toots.csv", "scraper/datasets")
-    
-    tfidf_vectorizer = TfidfVectorizer(max_df=1.0, min_df=1)
-
-    # Generate the tf-idf vectors for the corpus
-    content = test_toot_df['content']
-    tfidf_matrix = tfidf_vectorizer.fit_transform(content)
-
-    # Transform the sentence into a tf-idf vector
-    sentence_vector = tfidf_vectorizer.transform([sentence])
-
-    # Calculate the cosine similarity between the sentence vector and all toot vectors
-    cosine_sim_sentence = linear_kernel(sentence_vector, tfidf_matrix).flatten()
-
-    # Get the pairwise similarity scores
-    sim_scores = list(enumerate(cosine_sim_sentence))
-
-    # Sort the toots based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the scores for the specified number of most similar toots
-    sim_scores = sim_scores[:number_of_recommendations]
-    sim_indices = [i[0] for i in sim_scores]
-
-    # Return the recommended toots as a list of content
-    list_of_content = content.iloc[sim_indices]
-    return list_of_content.tolist()
 
 def normalize(scored_sentences):
-    max_score = scored_sentences[0]['cosine_sim']
-    min_score = scored_sentences[-1]['cosine_sim']
+    max_score = scored_sentences[0]["cosine_sim"]
+    min_score = scored_sentences[-1]["cosine_sim"]
     for index, entry in enumerate(scored_sentences):
         scored_sentences[index]["cosine_sim"] = (entry["cosine_sim"] - min_score) / (max_score - min_score)
     return scored_sentences
 
 
-def get_local_recommendations_with_tfidf(account_toots, followed_toots, result_set, number_of_recommendations=10):
+def get_local_recommendations_with_tfidf(account_toots, followed_toots, number_of_recommendations):
     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=1)
     # Generate the tf-idf vectors for the corpus
-    tfidf_matrix = tfidf_vectorizer.fit_transform(followed_toots)
+    tfidf_matrix = tfidf_vectorizer.fit_transform(followed_toots["preprocessed_content"].to_list())
     sentences_with_cosine_similarity = []
-    for sentence in account_toots:
-        sentence_vector = tfidf_vectorizer.transform([sentence])
+    for _, account_toot in account_toots.iterrows():
+        sentence_vector = tfidf_vectorizer.transform([account_toot["preprocessed_content"]])
         cosine_sim_sentence = linear_kernel(sentence_vector, tfidf_matrix).flatten()
-        #show_scores(sentence, followed_toots, cosine_sim_sentence)
-
+        # show_scores(sentence, followed_toots, cosine_sim_sentence)
         for index, cosine_sim in enumerate(cosine_sim_sentence):
             if len(sentences_with_cosine_similarity) < index + 1:
-                sentences_with_cosine_similarity.append({'sentence': result_set[index], 'preprocessed_sentence': followed_toots[index], 'cosine_sim': cosine_sim})
-            else:
-                sentences_with_cosine_similarity[index]['cosine_sim'] += cosine_sim
+                sentences_with_cosine_similarity.append(
+                    {
+                        "id": followed_toots.iloc[index]["id"],
+                        "sentence": followed_toots.iloc[index]["content"],
+                        "preprocessed_sentence": followed_toots.iloc[index]["preprocessed_content"],
+                        "cosine_sim": cosine_sim,
+                        "reblogs_count": float(followed_toots.iloc[index]["reblogs_count"]),
+                        "favourites_count": float(followed_toots.iloc[index]["favourites_count"]),
+                        "replies_count": float(followed_toots.iloc[index]["replies_count"]),
+                    }
+                )
 
-    sim_scores = sorted(sentences_with_cosine_similarity, key=lambda x: x['cosine_sim'], reverse=True)
+            else:
+                sentences_with_cosine_similarity[index]["cosine_sim"] += cosine_sim
+
+    sim_scores = sorted(sentences_with_cosine_similarity, key=lambda x: x["cosine_sim"], reverse=True)
     normalized_scores = normalize(sim_scores)
     return normalized_scores[:number_of_recommendations]
+
 
 def show_scores(sentence, followed_toots, cosine_sim_sentence):
     df = pd.DataFrame(cosine_sim_sentence)
@@ -82,42 +51,31 @@ def show_scores(sentence, followed_toots, cosine_sim_sentence):
     print("----")
     print(sentence)
     for index in df.index:
-        print("sentence matched: ", followed_toots[index], ", cosine_sim: ", cosine_sim_sentence[index])
+        print(
+            "sentence matched: ",
+            followed_toots[index],
+            ", cosine_sim: ",
+            cosine_sim_sentence[index],
+        )
     print("----")
 
-def get_account_id(data):
-    json_string = data.account.split(", \'display_name\'")[0] + "}";
-    json_string = json_string.replace("'", '"')
-    json_string = json_string.replace("False", "false")        
-    json_string = json_string.replace("True", "true")
-    json_object = json.loads(json_string)
-    data = data.drop("account")
-    return json_object['id']
-
-def get_account_toots(account_id):
-    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
-    path = f"api/v1/accounts/{account_id}/statuses/"
-    response = requests.get(url = f"https://{instance}/{path}")
-    
-    df = pd.DataFrame(data=response.json())
-    return df['content'].to_list() if len(df) > 0 else []
-
-def get_followed_accounts(account_id):
-    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
-    path = f"api/v1/accounts/{account_id}/following"
-    response = requests.get(url = f"https://{instance}/{path}")
-    
-    account_ids = pd.DataFrame(data=response.json()).id.to_list()
-    return account_ids
 
 def recommend_with_tfidf_for_account(account_id, number_of_recommendations=10):
     # get toots of user
     account_toots = get_account_toots(account_id)
-    preprocesses_account_toots = [sentence_preprocessing(sentence) for sentence in account_toots]
+    account_toots["preprocessed_content"] = account_toots["content"].apply(sentence_preprocessing)
     # get accounts followed by the user and collect relevant toots
     followed_accounts = get_followed_accounts(account_id)
-    followed_toots = [get_account_toots(account_id) for account_id in followed_accounts]
-    followed_toots = reduce(lambda x, y: x+y, followed_toots) 
-    preprocessed_followed_toots = [sentence_preprocessing(sentence) for sentence in followed_toots]
-    recommendations = get_local_recommendations_with_tfidf(preprocesses_account_toots, preprocessed_followed_toots, followed_toots, number_of_recommendations)
+    followed_toots = pd.DataFrame()
+    # concate dataframes
+    for account_id in followed_accounts:
+        followed_toots = pd.concat([followed_toots, get_account_toots(account_id)])
+
+    followed_toots["preprocessed_content"] = followed_toots["content"].apply(sentence_preprocessing)
+
+    recommendations = get_local_recommendations_with_tfidf(
+        account_toots,
+        followed_toots,
+        number_of_recommendations,
+    )
     return recommendations
---
+++ recommender_api/utils/tfidf_demo.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import os
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+import requests
+from functools import reduce
+from .preprocessing import sentence_preprocessing
+
+
+def load_test_data(filename, path):
+    data = pd.read_csv(os.path.join(path, filename), sep=";")
+
+    # select only english and german toots
+    mask_language = (data["language"] == "en") | (data["language"] == "de")
+    data = data[mask_language]
+
+    return data
+
+
+def recommend_with_tfidf(sentence, number_of_recommendations=10):
+    test_toot_df = load_test_data("mastodon.social_toots.csv", "scraper/datasets")
+
+    tfidf_vectorizer = TfidfVectorizer(max_df=1.0, min_df=1)
+
+    # Generate the tf-idf vectors for the corpus
+    content = test_toot_df["content"]
+    tfidf_matrix = tfidf_vectorizer.fit_transform(content)
+
+    # Transform the sentence into a tf-idf vector
+    sentence_vector = tfidf_vectorizer.transform([sentence])
+
+    # Calculate the cosine similarity between the sentence vector and all toot vectors
+    cosine_sim_sentence = linear_kernel(sentence_vector, tfidf_matrix).flatten()
+
+    # Get the pairwise similarity scores
+    sim_scores = list(enumerate(cosine_sim_sentence))
+
+    # Sort the toots based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores for the specified number of most similar toots
+    sim_scores = sim_scores[:number_of_recommendations]
+    sim_indices = [i[0] for i in sim_scores]
+
+    # Return the recommended toots as a list of content
+    list_of_content = content.iloc[sim_indices]
+    return list_of_content.tolist()
+
+
+def normalize(scored_sentences):
+    max_score = scored_sentences[0]["cosine_sim"]
+    min_score = scored_sentences[-1]["cosine_sim"]
+    for index, entry in enumerate(scored_sentences):
+        scored_sentences[index]["cosine_sim"] = (entry["cosine_sim"] - min_score) / (
+            max_score - min_score
+        )
+    return scored_sentences
+
+
+def get_local_recommendations_with_tfidf(
+    account_toots, followed_toots, result_set, number_of_recommendations=10
+):
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=1)
+    # Generate the tf-idf vectors for the corpus
+    tfidf_matrix = tfidf_vectorizer.fit_transform(followed_toots)
+    sentences_with_cosine_similarity = []
+    for sentence in account_toots:
+        sentence_vector = tfidf_vectorizer.transform([sentence])
+        cosine_sim_sentence = linear_kernel(sentence_vector, tfidf_matrix).flatten()
+        # show_scores(sentence, followed_toots, cosine_sim_sentence)
+
+        for index, cosine_sim in enumerate(cosine_sim_sentence):
+            if len(sentences_with_cosine_similarity) < index + 1:
+                sentences_with_cosine_similarity.append(
+                    {
+                        "sentence": result_set[index],
+                        "preprocessed_sentence": followed_toots[index],
+                        "cosine_sim": cosine_sim,
+                    }
+                )
+            else:
+                sentences_with_cosine_similarity[index]["cosine_sim"] += cosine_sim
+
+    sim_scores = sorted(
+        sentences_with_cosine_similarity, key=lambda x: x["cosine_sim"], reverse=True
+    )
+    normalized_scores = normalize(sim_scores)
+    return normalized_scores[:number_of_recommendations]
+
+
+def show_scores(sentence, followed_toots, cosine_sim_sentence):
+    df = pd.DataFrame(cosine_sim_sentence)
+    mask = df[0] > 0
+    df = df[mask]
+    print("----")
+    print(sentence)
+    for index in df.index:
+        print(
+            "sentence matched: ",
+            followed_toots[index],
+            ", cosine_sim: ",
+            cosine_sim_sentence[index],
+        )
+    print("----")
+
+
+def get_account_id(data):
+    json_string = data.account.split(", 'display_name'")[0] + "}"
+    json_string = json_string.replace("'", '"')
+    json_string = json_string.replace("False", "false")
+    json_string = json_string.replace("True", "true")
+    json_object = json.loads(json_string)
+    data = data.drop("account")
+    return json_object["id"]
+
+
+def get_account_toots(account_id):
+    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
+    path = f"api/v1/accounts/{account_id}/statuses/"
+    response = requests.get(url=f"https://{instance}/{path}")
+
+    df = pd.DataFrame(data=response.json())
+    return df["content"].to_list() if len(df) > 0 else []
+
+
+def get_followed_accounts(account_id):
+    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
+    path = f"api/v1/accounts/{account_id}/following"
+    response = requests.get(url=f"https://{instance}/{path}")
+
+    account_ids = pd.DataFrame(data=response.json()).id.to_list()
+    return account_ids
+
+
+def recommend_with_tfidf_for_account(account_id, number_of_recommendations=10):
+    # get toots of user
+    account_toots = get_account_toots(account_id)
+    preprocesses_account_toots = [
+        sentence_preprocessing(sentence) for sentence in account_toots
+    ]
+    # get accounts followed by the user and collect relevant toots
+    followed_accounts = get_followed_accounts(account_id)
+    followed_toots = [get_account_toots(account_id) for account_id in followed_accounts]
+    followed_toots = reduce(lambda x, y: x + y, followed_toots)
+    preprocessed_followed_toots = [
+        sentence_preprocessing(sentence) for sentence in followed_toots
+    ]
+    recommendations = get_local_recommendations_with_tfidf(
+        preprocesses_account_toots,
+        preprocessed_followed_toots,
+        followed_toots,
+        number_of_recommendations,
+    )
+    return recommendations

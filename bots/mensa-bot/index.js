8b345ae960f4473416b19343e68ddbc2137e2cf0
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-06-17 17:09:01 +0200

    Updated mensa bot
    - excluded pizza
    - now only posts one toot with all foods instead of one post for each food

--- bots/mensa-bot/index.js
+++ bots/mensa-bot/index.js
@@ -7,26 +7,29 @@ import getTodaysMeals from './mensa.js';
   try {
     dotenv.config();
     const meals = await getTodaysMeals();
-    if (!meals) return;
+    if (!meals.length) return;
     const masto = await login({
       url: process.env.MASTODON_URL,
       accessToken: process.env.MENSA_BOT_ACCESS_TOKEN,
-      timeout: 5000
+      timeout: 5000,
     });
 
-    let status;
+    let status = '';
+    let mediaIds = [];
     for (const meal of meals) {
       const imageRaw = await axios.get(meal.PFAD, { responseType: 'arraybuffer' });
       const image = Buffer.from(imageRaw.data, 'binary');
-      const media = await masto.v2.mediaAttachments.create({ file: image });
-      status = await masto.v1.statuses.create({
-        status: `${meal.SPEISE}: ${meal.AUSGABETEXT}\nStudierende: ${meal.STUDIERENDE} € , Bedienstete: ${meal.BEDIENSTETE} € , Gäste: ${meal.GAESTE} €`,
-        inReplyToId: status?.id,
-        mediaIds: [media.id],
-        language: 'de'
-      });
+      let media = await masto.v2.mediaAttachments.create({ file: image, description: meal.SPEISE });
+      mediaIds.push(media.id);
+      status += `${meal.SPEISE}: ${meal.AUSGABETEXT.replace(/ \([^)]*\) */g, ' ').trim()}\nStudierende: ${meal.STUDIERENDE}€, Bedienstete: ${meal.BEDIENSTETE}€, Gäste: ${meal.GAESTE}€${meal !== meals[meals.length - 1] ? '\n\n': ''}`;
     }
+
+    await masto.v1.statuses.create({
+      status,
+      mediaIds,
+      language: 'de',
+    });
   } catch (error) {
     console.error(error);
   }
-})()
+})();
--- bots/mensa-bot/mensa.js
+++ bots/mensa-bot/mensa.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as xml2js from 'xml2js';
 
-const EXCLUDED_FOOD = ['zur Pizza', 'Salatbuffet', 'Dessertauswahl', 'Beilagenauswahl'];
+const EXCLUDED_FOOD = ['Pizza', 'zur Pizza', 'Salatbuffet', 'Dessertauswahl', 'Beilagenauswahl'];
 
 export default async function () {
   let meals;
@@ -9,10 +9,9 @@ export default async function () {
   const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true });
   parser.parseString(res.data, (err, result) => {
     meals = result.DATAPACKET.ROWDATA.ROW.filter((entry =>
-      entry.MENSA == process.env.MENSA_NAME &&
+      entry.MENSA === process.env.MENSA_NAME &&
       entry.DATUM === new Date().toLocaleString('de-DE', { dateStyle: 'medium' }) &&
       !EXCLUDED_FOOD.includes(entry.SPEISE)));
   });
-  meals.sort(((a, b) => a.SPEISE === 'Pizza' ? 1 : (b.SPEISE === 'Pizza' ? -1 : 0)))
   return meals;
 }

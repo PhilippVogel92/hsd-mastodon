b7750f424b9d3670032068929851c097f7a8275a
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-06-19 09:13:03 +0000

    Update create-account.sh

--- bots/create-account.sh
+++ bots/create-account.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Script must be executed as the mastodon user!
 # Creates a new account without having to confirm the email
 # The generated password is printed at the end of the script
 read -p 'Account name (tag, not display name): ' username
 read -p 'E-Mail: ' email
-sudo -u mastodon RAILS_ENV=production /home/mastodon/live/bin/tootctl accounts create "$username" --email "$email" --confirmed
+RAILS_ENV=production /home/mastodon/live/bin/tootctl accounts create "$username" --email "$email" --confirmed

846a7d5aa4de88dc1fa6f0034e1c7abec1174665
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-06-20 14:13:53 +0200

    recover old version of the tfidf demo

--- recommender_api/utils/tfidf_demo.py
+++ recommender_api/utils/tfidf_demo.py
@@ -1,88 +1,52 @@
-import atexit
-import json
 import os
 from functools import reduce
-
+import requests
+import json
 import pandas as pd
-import psycopg2
-from dotenv import load_dotenv
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-from .preprocessing import sentence_preprocessing
-
-# Load database connection parameters
-load_dotenv()
-
-# Connect to the database
-conn = psycopg2.connect(host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"), dbname=os.getenv("DB_NAME"),
-                        user=os.getenv("DB_USER"), password=os.getenv("DB_PASS"))
+from .preprocessing import TextPreprocessor
 
 
-def cleanup():
-    """
-    Close the database connection when the program is terminated.
-    """
-    conn.close()
+def get_account_id(data):
+    json_string = data.account.split(", 'display_name'")[0] + "}"
+    json_string = json_string.replace("'", '"')
+    json_string = json_string.replace("False", "false")
+    json_string = json_string.replace("True", "true")
+    json_object = json.loads(json_string)
+    data = data.drop("account")
+    return json_object["id"]
 
 
-# Register the cleanup function to be called when the program is terminated
-atexit.register(cleanup)
+def get_account_toots(account_id):
+    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
+    path = f"api/v1/accounts/{account_id}/statuses/"
+    response = requests.get(url=f"https://{instance}/{path}")
+
+    df = pd.DataFrame(data=response.json())
+    return df["content"].to_list() if len(df) > 0 else []
 
 
-def load_test_data(filename, path):
-    data = pd.read_csv(os.path.join(path, filename), sep=";")
+def get_followed_accounts(account_id):
+    instance = "mastodon.hosting.medien.hs-duesseldorf.de"
+    path = f"api/v1/accounts/{account_id}/following"
+    response = requests.get(url=f"https://{instance}/{path}")
 
-    # select only english and german toots
-    mask_language = (data["language"] == "en") | (data["language"] == "de")
-    data = data[mask_language]
-
-    return data
-
-
-def recommend_with_tfidf(sentence, number_of_recommendations=10):
-    test_toot_df = load_test_data("mastodon.social_toots.csv", "scraper/datasets")
-
-    tfidf_vectorizer = TfidfVectorizer(max_df=1.0, min_df=1)
-
-    # Generate the tf-idf vectors for the corpus
-    content = test_toot_df["content"]
-    tfidf_matrix = tfidf_vectorizer.fit_transform(content)
-
-    # Transform the sentence into a tf-idf vector
-    sentence_vector = tfidf_vectorizer.transform([sentence])
-
-    # Calculate the cosine similarity between the sentence vector and all toot vectors
-    cosine_sim_sentence = linear_kernel(sentence_vector, tfidf_matrix).flatten()
-
-    # Get the pairwise similarity scores
-    sim_scores = list(enumerate(cosine_sim_sentence))
-
-    # Sort the toots based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the scores for the specified number of most similar toots
-    sim_scores = sim_scores[:number_of_recommendations]
-    sim_indices = [i[0] for i in sim_scores]
-
-    # Return the recommended toots as a list of content
-    list_of_content = content.iloc[sim_indices]
-    return list_of_content.tolist()
+    account_ids = pd.DataFrame(data=response.json()).id.to_list()
+    return account_ids
 
 
 def normalize(scored_sentences):
     max_score = scored_sentences[0]["cosine_sim"]
     min_score = scored_sentences[-1]["cosine_sim"]
     for index, entry in enumerate(scored_sentences):
-        scored_sentences[index]["cosine_sim"] = (entry["cosine_sim"] - min_score) / (
-            max_score - min_score
-        )
+        scored_sentences[index]["cosine_sim"] = (entry["cosine_sim"] - min_score) / (max_score - min_score)
     return scored_sentences
 
 
-def get_local_recommendations_with_tfidf(
-    account_toots, followed_toots, result_set, number_of_recommendations=10
-):
+def get_local_recommendations_with_tfidf(account_toots, followed_toots, result_set, number_of_recommendations=10):
     tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=1)
     # Generate the tf-idf vectors for the corpus
     tfidf_matrix = tfidf_vectorizer.fit_transform(followed_toots)
@@ -104,9 +68,7 @@ def get_local_recommendations_with_tfidf(
             else:
                 sentences_with_cosine_similarity[index]["cosine_sim"] += cosine_sim
 
-    sim_scores = sorted(
-        sentences_with_cosine_similarity, key=lambda x: x["cosine_sim"], reverse=True
-    )
+    sim_scores = sorted(sentences_with_cosine_similarity, key=lambda x: x["cosine_sim"], reverse=True)
     normalized_scores = normalize(sim_scores)
     return normalized_scores[:number_of_recommendations]
 
@@ -127,59 +89,18 @@ def show_scores(sentence, followed_toots, cosine_sim_sentence):
     print("----")
 
 
-def get_account_id(data):
-    json_string = data.account.split(", 'display_name'")[0] + "}"
-    json_string = json_string.replace("'", '"')
-    json_string = json_string.replace("False", "false")
-    json_string = json_string.replace("True", "true")
-    json_object = json.loads(json_string)
-    data.drop("account")
-    return json_object["id"]
-
-
-def get_account_toots(account_id):
-    """
-    Get all toots of an account.
-
-    :param account_id: The id of the account.
-    :return: A list of toots.
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT text FROM statuses WHERE account_id = %s;", (account_id,))
-    toots = cur.fetchall()
-    cur.close()
-    return [toot[0] for toot in toots]
-
-
-def get_followed_accounts(account_id):
-    """
-    Get all accounts followed by an account.
-
-    :param account_id: The id of the account.
-    :return: A list of account ids.
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT target_account_id FROM follows WHERE account_id = %s;", (account_id,))
-    follows = cur.fetchall()
-    cur.close()
-    return [follow[0] for follow in follows]
-
-
-def recommend_with_tfidf_for_account(account_id, number_of_recommendations=10):
+def recommend_with_tfidf_for_account(account_id, nlp_model_loader, number_of_recommendations=10):
     # get toots of user
     account_toots = get_account_toots(account_id)
-    preprocesses_account_toots = [
-        sentence_preprocessing(sentence) for sentence in account_toots
-    ]
+    preprocessor = TextPreprocessor(nlp_model_loader)
+    preprocessed_account_toots = [preprocessor.sentence_preprocessing(sentence) for sentence in account_toots]
     # get accounts followed by the user and collect relevant toots
     followed_accounts = get_followed_accounts(account_id)
     followed_toots = [get_account_toots(account_id) for account_id in followed_accounts]
     followed_toots = reduce(lambda x, y: x + y, followed_toots)
-    preprocessed_followed_toots = [
-        sentence_preprocessing(sentence) for sentence in followed_toots
-    ]
+    preprocessed_followed_toots = [preprocessor.sentence_preprocessing(sentence) for sentence in followed_toots]
     recommendations = get_local_recommendations_with_tfidf(
-        preprocesses_account_toots,
+        preprocessed_account_toots,
         preprocessed_followed_toots,
         followed_toots,
         number_of_recommendations,

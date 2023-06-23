b0eb29264aea4cda8df1f1d17ef231bdc755e1f4
Ben Kräling <kraelibe@gmail.com>
2023-06-23 18:33:51 +0200

    Fix home timeline

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -9,18 +9,4 @@ class HomeFeed < Feed
   def regenerating?
     redis.exists?("account:#{@account.id}:regeneration")
   end
-
-  def get(limit, max_id = nil, since_id = nil, min_id = nil)
-    limit    = limit.to_i
-    max_id   = max_id.to_i if max_id.present?
-    since_id = since_id.to_i if since_id.present?
-    min_id   = min_id.to_i if min_id.present?
-
-    status = from_redis(limit, max_id, since_id, min_id)
-
-    status.sort()
-    # reorder 
-  end
-
-
 end

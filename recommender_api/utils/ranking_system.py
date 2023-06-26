afc81307bc6bbfda8813e4efe15c9dc5c19be8b2
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-06-26 17:36:58 +0200

    improve ranking-system and sort timeline

--- recommender_api/utils/ranking_system.py
+++ recommender_api/utils/ranking_system.py
@@ -1,78 +1,163 @@
 from .tfidf import TFIDFRecommender
-from .spacy_sim import recommend_with_spacy_for_account
 import pandas as pd
+import numpy as np
+from recommender_api.model.status_queries import (
+    get_status_with_tag_ids_and_stats_by_status_id,
+    get_status_stats_by_status_id,
+)
+
+from recommender_api.model.tag_queries import get_tags_by_account_id
 
 
 class RankingSystem:
-    """Class to calculate the ranking score of a status."""
+    """Class to create a ranking system for statuses and sort the timeline of an account."""
 
-    def __init__(
+    def __init__(self):
+        pass
+
+    def are_account_tags_in_status(self, status, tag_ids_from_account):
+        """
+        Function to check if the tags of an account are in a status.
+        param status: A specific status by id with joined tag_id and stats.
+        param tag_ids_from_account: A list of tag ids from an account.
+        return: True if the tags of an account are in a status, else False.
+        """
+        found_status_tags_in_account_tags = False
+
+        for tag_id in status["tag_ids"]:
+            if tag_id and tag_id in tag_ids_from_account:
+                found_status_tags_in_account_tags = True
+                print(tag_id, "is in account tags")
+
+        return found_status_tags_in_account_tags
+
+    def calculate_ranking_score(
         self,
-        nlp_model_loader,
-        similarity_weight=0.5,
-        interaction_weight=0.5,
-        number_of_recommendations=20,
-        similarity_concept="tfidf",
-        sort_by_ranking_score=True,
+        status,
+        tag_ids_from_account,
+        weight_for_favourites_count=1.5,
+        weight_for_replies_count=15.0,
+        weight_for_reblogs_count=2.0,
+        weight_for_time=1.1,
+        boost_for_tags=100,
     ):
-        self.similarity_weight = similarity_weight
-        self.interaction_weight = interaction_weight
-        self.sort_by_ranking_score = sort_by_ranking_score
-        self.number_of_recommendations = number_of_recommendations
-        self.similarity_concept = similarity_concept
-        self.nlp_model_loader = nlp_model_loader
+        """
+        Function to calculate the ranking score of a status.
 
-    def set_similarity_weight(self, weight):
-        self.similarity_weight = weight
+        :param status: A specific status by id with joined tag_id and stats.
+        :param weight_for_favourites_count: Weight for favourites count.
+        :param weight_for_replies_count: Weight for replies count.
+        :param weight_for_reblogs_count: Weight for reblogs count.
+        :param weight_for_time: Weight for time.
+        :return: A specific status with a ranking score.
 
-    def set_interaction_weight(self, weight):
-        self.interaction_weight = weight
+        """
 
-    def calculate_interaction_score(self, status_df):
-        """Function to calculate the interaction score of a status."""
+        favourites_count = status["favourites_count"]
+        replies_count = status["replies_count"]
+        reblogs_count = status["reblogs_count"]
+        updated_at = status["updated_at"]
 
-        # Calculate the interaction score
-        status_df["interaction_score"] = (
-            status_df["favourites_count"] + status_df["replies_count"] + status_df["reblogs_count"]
+        # calculate the time in days since the status was updated
+        days_since_status_update = (pd.Timestamp.now() - updated_at).days
+
+        # Logging for debugging
+        print(
+            "------- Start calculating ranking score for status:",
+            status["id"],
+            "-------",
+        )
+        print("Days since status update:", days_since_status_update)
+        print("Favourites count:", favourites_count)
+        print("Replies count:", replies_count)
+        print("Reblogs count:", reblogs_count)
+        print("Tags in account:", tag_ids_from_account)
+        print("Tags in status:", status["tag_ids"])
+
+        interaction_weight = 1
+
+        # Check if the status has any interactions
+        if replies_count and reblogs_count and favourites_count:
+            # Prevent log(0) error
+            if replies_count != 0 or reblogs_count != 0 or favourites_count != 0:
+                interaction_weight = np.log(
+                    weight_for_replies_count * replies_count
+                    + weight_for_favourites_count * favourites_count
+                    + weight_for_reblogs_count * reblogs_count
+                )
+
+        print("Interaktion_weight:", interaction_weight)
+
+        # Calculation of ranking score with a log function
+        ranking_score = interaction_weight * (
+            1 / (np.log(weight_for_time * (days_since_status_update + 1)))
         )
 
-        # Normalize the interaction score to the value range [0, 1]
-        max_interaction_score = status_df["interaction_score"].max()
-        status_df["interaction_score"] = status_df["interaction_score"] / max_interaction_score
+        # Flat boost for tags in account if the account has minimum one tag
+        if tag_ids_from_account and self.are_account_tags_in_status(
+            status, tag_ids_from_account
+        ):
+            ranking_score = ranking_score + boost_for_tags
+            print("Status boosted because of tags in account.")
 
-        return status_df
+        # Add ranking score to status
+        status["ranking_score"] = ranking_score
 
-    def calculate_ranking_score(self, status_df):
-        """Function to calculate the ranking score of a status."""
+        print("Status with ranking score:", status)
+        print("------- End calculating ranking score for status -------")
+        return status
 
-        # Calculate the ranking score
-        status_df["ranking_score"] = (self.similarity_weight * status_df["cosine_sim"]) + (
-            self.interaction_weight * status_df["interaction_score"]
+    def sort_timeline(
+        self, account_id, status_ids, nlp_model_loader, number_of_recommendations=20
+    ):
+        """Function to sort the timeline of a account by ranking score.
+        param account_id: The id of the account.
+        param status_ids: The ids of the statuses.
+        param nlp_model_loader: The nlp model loader.
+        param number_of_recommendations: The number of recommendations.
+        return: A list of sorted status ids by ranking score.
+        """
+
+        # Mock Data
+        status_ids = [
+            "110589714508600527",
+            "110594975367510259",
+            "110595013146537691",
+            "110611101938508635",
+        ]
+
+        # Get statuses with tag ids and stats
+        statuses_with_tag_ids_and_stats = [
+            get_status_with_tag_ids_and_stats_by_status_id(status_id)
+            for status_id in status_ids
+        ]
+
+        # Set Limit of recommendations
+        statuses_with_tag_ids_and_stats = statuses_with_tag_ids_and_stats[
+            :number_of_recommendations
+        ]
+
+        # Get tag ids from account
+        tag_ids_from_account = get_tags_by_account_id(account_id)
+
+        # Calculate ranking score for each status
+        ranked_statuses = [
+            self.calculate_ranking_score(status, tag_ids_from_account)
+            for status in statuses_with_tag_ids_and_stats
+        ]
+
+        # Sort statuses by ranking score
+        sorted_statuses = sorted(
+            ranked_statuses, key=lambda x: x["ranking_score"], reverse=True
         )
 
-        if self.sort_by_ranking_score:
-            # Sort the DataFrame according to the ranking score (descending)
-            status_df.sort_values("ranking_score", ascending=False, inplace=True)
-            status_df.reset_index(drop=True, inplace=True)
+        # Get status ids from sorted statuses
+        sorted_statuses_ids = [status["id"] for status in sorted_statuses]
 
-        return status_df
+        """ 
+        # Example usage of the TFIDFRecommender
+        tfidf_recommender = TFIDFRecommender(number_of_recommendations, nlp_model_loader)
+        recommendations_for_account = tfidf_recommender.recommend_statuses_from_follower(account_id)
+        """
 
-    def get_recommendations_with_ranking_system(self, account_id):
-        if self.similarity_concept == "tfidf":
-            tfidf_recommender = TFIDFRecommender(self.number_of_recommendations, self.nlp_model_loader)
-            recommendations_for_account = tfidf_recommender.recommend_statuses_from_follower(account_id)
-        elif self.similarity_concept == "spacy":
-            recommendations_for_account = recommend_with_spacy_for_account(account_id, self.number_of_recommendations)
-        else:
-            raise ValueError(f"Invalid similarity concept: {self.similarity_concept}")
-
-        # transform recommendations_for_account to a DataFrame
-        recommendations_for_account_df = pd.DataFrame(recommendations_for_account)
-
-        # Calculate the interaction score and expand the DataFrame
-        recommendations_for_account_df = self.calculate_interaction_score(recommendations_for_account_df)
-
-        # Calculate the ranking score, expand the DataFrame and sort the DataFrame by ranking score
-        recommendations_with_ranking = self.calculate_ranking_score(recommendations_for_account_df)
-
-        return recommendations_with_ranking.to_dict(orient="records")
+        return sorted_statuses_ids

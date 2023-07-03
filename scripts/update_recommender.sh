3ab8c803b628c0b983bd26eec131197aaaec47ca
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2023-07-03 16:46:25 +0200

    Added recommender update script

---
+++ scripts/update_recommender.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script updates the dependencies of the recommendation algorithm.
+#
+# ** It should be run as the mastodon user. **
+# ** It assumes that the Mastodon live server and the recommendation API is stopped before running this. **
+# ** It assumes that the recommender is deployed inside of a virtual environment located at /home/mastodon/live/recommender_api/venv. **
+
+set -e # Stop on error
+
+echo "Updating the Recommmender API."
+
+# Updating Python dependencies
+pushd /home/mastodon/live/recommender_api
+source "./venv/bin/activate"
+pip install -r requirements.txt
+deactivate
+popd
+
+echo "Finished updating Recommmender API."

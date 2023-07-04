a861446963467309144f8e1b4621661dcef9984f
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-07-04 18:26:49 +0200

    move files to service folder

---
+++ recommender_api/services/hashtag_modelling.py
@@ -0,0 +1,105 @@
+from .preprocessing import TextPreprocessor
+from ..model.status_queries import persist_status_tag_relation
+from ..model.tag_queries import get_all_tags_with_name_and_id, get_tags_by_status_id
+from langdetect import detect
+from spacy.tokens import Doc
+import time
+
+
+class TagGenerator:
+    """Class to extract keywords from a text and compare them with hashtags."""
+
+    def __init__(self, status, nlp_model_loader, treshold=0.5):
+        self.nlp_model_loader = nlp_model_loader
+        self.nlp = self.choose_nlp_model(status)
+        self.treshold = treshold
+        self.status = status
+
+    def choose_nlp_model(self, status):
+        """Function to choose the right NLP model."""
+        language = detect(status["text"])
+        nlp_model = "de_core_news_lg"
+        if language == "en":
+            nlp_model = "en_core_web_lg"
+            print("English language detected.")
+
+        nlp = self.nlp_model_loader.get_model(nlp_model)
+        print("Loaded NLP model:", nlp.meta["lang"] + "_" + nlp.meta["name"])
+        return nlp
+
+    def extract_keywords(self, text):
+        """Function to extract keywords from a text."""
+        doc = self.nlp(text)
+
+        keywords = []
+
+        # Extract keywords from text
+        for token in doc:
+            if token.pos_ in ["VERB", "NOUN", "PROPN"]:
+                if token.text not in keywords:
+                    keywords.append(token.text)
+
+        return keywords
+
+    def match_hashtags_with_status(self, hashtags):
+        """Function to match hashtags with text."""
+        matches = []
+        status_text = self.status["preprocessed_content"]
+        status_id = self.status["id"]
+        keywords = self.extract_keywords(status_text)
+        print("Input Text:", status_text, "Keywords found:", keywords)
+
+        for keyword_doc in self.nlp.pipe(keywords):
+            for hashtag_doc, hashtag_id in self.nlp.pipe(hashtags, as_tuples=True):
+                hashtag_name = hashtag_doc.text
+                similarity = keyword_doc.similarity(hashtag_doc)
+
+                print(
+                    "Schlagwort:",
+                    keyword_doc.text,
+                    "  Hashtag:",
+                    hashtag_name,
+                    "  Ähnlichkeit:",
+                    similarity,
+                )
+
+                if similarity >= self.treshold:
+                    if hashtag_name not in matches:
+                        matches.append(hashtag_name)
+                        persist_status_tag_relation(status_id, hashtag_id)
+        return matches
+
+    def generate_hashtags(self):
+        """Function to generate hashtags for a status."""
+
+        # timer which stops the second from start to end
+        start = time.time()
+
+        # get all hashtags from database
+        hashtags = get_all_tags_with_name_and_id()
+
+        status_tags = get_tags_by_status_id(self.status["id"])
+
+        hashtags = [hashtag for hashtag in hashtags if hashtag[0] not in status_tags]
+
+        # initialize text preprocessor
+        nlp_model_name = self.nlp.meta["lang"] + "_" + self.nlp.meta["name"]
+        text_preprocessor = TextPreprocessor(self.nlp_model_loader, nlp_model_name)
+
+        # preprocess text and clean it from html tags, urls, newlines, etc.
+        self.status["preprocessed_content"] = text_preprocessor.sentence_preprocessing(
+            self.status["text"]
+        )
+        print("Text:", self.status["preprocessed_content"])
+
+        # extract keywords from text
+        tags = self.match_hashtags_with_status(hashtags)
+        print("Tags:", tags)
+
+        # add hashtags to status
+        self.status["tags"] = tags
+
+        end = time.time()
+        diff = end - start
+        print("------------Time Difference:", diff, "-----------------")
+        return self.status["tags"]
---
+++ recommender_api/services/preprocessing.py
@@ -0,0 +1,57 @@
+import re
+import html2text
+
+
+class TextPreprocessor:
+    """Class to preprocess text data."""
+
+    def __init__(self, nlp_model_loader, status=None, nlp_model="de_core_news_lg"):
+        self.nlp = nlp_model_loader.get_model(nlp_model)
+        self.status = status
+
+    def replace_newlines_in_url(self, match):
+        url = match.group()
+        return url.replace("\n", "")
+
+    def remove_tag_urls(self, sentence):
+        url_regex = r"(?<=\])\(https?://[^\s/$.?#].[^\[\]\(\)]*(?=\))\)"
+        return re.sub(url_regex, "", sentence)
+
+    def remove_newlines_in_text(self, sentence):
+        return sentence.replace("\n", " ")
+
+    def lemmatize_text(self, sentence):
+        """Function to lemmatize text data and remove the stopwords."""
+        doc = self.nlp(sentence)
+        # Lemmatization and removal of stop words
+        processed_tokens = [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
+        # Return the formatted text as a string
+        processed_text = " ".join(processed_tokens)
+        return processed_text
+
+    def lower_words(self, sentence):
+        return sentence.lower()
+
+    def remove_html_tags(self, sentence):
+        return html2text.html2text(sentence)
+
+    def sentence_preprocessing(self, sentence):
+        sentence = self.remove_html_tags(sentence)
+        sentence = self.remove_newlines_in_text(sentence)
+        sentence = self.remove_tag_urls(sentence)
+        sentence = self.lower_words(sentence)
+        sentence = self.lemmatize_text(sentence)
+        return sentence
+
+    def status_preprocessing(self):
+        if self.status is None:
+            raise Exception("No status provided.")
+
+        sentence = self.status["content"]
+        sentence = self.remove_html_tags(sentence)
+        sentence = self.remove_newlines_in_text(sentence)
+        sentence = self.remove_tag_urls(sentence)
+        sentence = self.lower_words(sentence)
+        sentence = self.lemmatize_text(sentence)
+        self.status["preprocessed_content"] = sentence
+        return self.status

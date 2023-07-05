f5d48077186a12a3798175d1947579f386d133a1
Kevin Zielke <kevin_zielke@yahoo.de>
2023-07-05 23:04:26 +0200

    added new ranking score calculation for toots.

--- recommender_api/services/ranking_system.py
+++ recommender_api/services/ranking_system.py
@@ -9,6 +9,28 @@ from recommender_api.model.account_queries import get_followed_accounts, get_blo
 
 class RankingSystem:
     """Class to create a ranking system for statuses and sort the timeline of an account."""
+    
+    # if we might gain more users, we can scale the ranking score with this factor
+    # low values cause higher variance on low interaction counts
+    scalibility_factor = 10.0
+
+    # interaction weights: set to whatever feels right. Will be normalized anyway ;)
+    replies_weight = 15.0
+    favourites_weight = 1.5
+    reblogs_weight = 2.0
+    
+    # Not normalized. Higher values cause a higher loss in ranking score over time
+    age_weight = 1.0
+
+    # normalized interaction weights
+    comments_weight_normalized = 0.0
+    favourites_weight_normalized = 0.0
+    boost_weight_normalized = 0.0
+
+    # flat boosts added to ranking score, if they apply
+    boost_for_following = 0.2
+    boost_for_tags = 0.05
+    boost_for_blocked = -1000
 
     def __init__(self):
         pass
@@ -86,8 +108,83 @@ class RankingSystem:
                 return True
             else:
                 return False
+            
+    def normalize_counts(self, count):
+        return 1 - (1 / (1 + np.log1p(count / self.scalibility_factor)))
 
-    def calculate_ranking_score(
+    def normalize_age(self, count):
+        return 1 / (1 + np.log1p(count * self.age_weight))
+
+    def normalize_interaction_weights(self):
+        # Calculate the sum of the input values
+        weight_sum = self.replies_weight + self.favourites_weight + self.reblogs_weight
+
+        # Scale the values proportionally
+        self.comments_weight_normalized = self.replies_weight / weight_sum
+        self.favourites_weight_normalized = self.favourites_weight / weight_sum
+        self.boost_weight_normalized = self.reblogs_weight / weight_sum
+        
+    def calculate_ranking_score(self,
+        status,
+        tag_ids_from_account,
+        following_list,
+        muted_list,
+        blocked_list,
+    ):
+        """
+        Function to calculate the ranking score of a status.
+
+        :param status: A specific status by id with joined tag_id and stats.
+        :param weight_for_favourites_count: Weight for favourites count.
+        :param weight_for_replies_count: Weight for replies count.
+        :param weight_for_reblogs_count: Weight for reblogs count.
+        :param weight_for_time: Weight for time.
+        :return: A specific status with a ranking score.
+        """
+
+        favourites_count = status["favourites_count"]
+        replies_count = status["replies_count"]
+        reblogs_count = status["reblogs_count"]
+        updated_at = status["updated_at"]
+
+        # calculate the time in days since the status was updated
+        days_since_status_update = (pd.Timestamp.now() - updated_at).days
+        
+        # maybe this can/should be done in __init__?
+        self.normalize_interaction_weights()
+        
+        # calculate weighted interaction subscores
+        replies_score = self.normalize_counts(replies_count) * self.comments_weight_normalized
+        favourites_score = self.normalize_counts(favourites_count) * self.favourites_weight_normalized
+        reblogs_score = self.normalize_counts(reblogs_count) * self.boost_weight_normalized
+        
+        # calculate ranking score based on weighted interactions
+        ranking_score = replies_score + favourites_score + reblogs_score
+        
+        # add boost if status contains followed hashtags
+        if tag_ids_from_account:
+            ranking_score += self.count_account_tags_in_status(status, tag_ids_from_account) * self.boost_for_tags
+
+        # add boost if user follows th status author
+        if self.check_if_author_is_followed(status, following_list):
+            ranking_score += self.boost_for_following
+        
+        # surpress blocked or muted authors
+        if self.check_if_author_is_blocked(status, blocked_list) or self.check_if_author_is_muted(status, muted_list):
+            ranking_score += self.boost_for_blocked
+        
+        # calculate age score
+        age_score = self.normalize_age(days_since_status_update)
+
+        # calculate final ranking score based on age of the status
+        ranking_score = ranking_score * age_score
+        
+        # Add ranking score to status
+        status["ranking_score"] = ranking_score
+        
+        return status    
+
+    def calculate_ranking_score_old(
         self,
         status,
         tag_ids_from_account,

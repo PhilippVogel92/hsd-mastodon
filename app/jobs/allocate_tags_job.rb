f997f3796f0ae0ba6eab8906323647ffee2dc962
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-07-07 17:58:20 +0200

    change endpoint to get

--- app/jobs/allocate_tags_job.rb
+++ app/jobs/allocate_tags_job.rb
@@ -2,8 +2,7 @@ class AllocateTagsJob < ApplicationJob
   queue_as :default
 
   def perform(status_id)
-    headers = {'Content-Type': 'application/json'}
-    Net::HTTP.post(URI(ENV['RECOMMENDER_URL'] + "/statuses/#{status_id}/generate-tags"), nil, headers)
+    Net::HTTP.get(URI(ENV['RECOMMENDER_URL'] + "/statuses/#{status_id}/generate-tags"))
   end
 
   def max_attempts

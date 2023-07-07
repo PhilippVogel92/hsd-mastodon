76a97b254fed3c17837f2e8e81aa8d73f4776501
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-07-07 17:58:48 +0200

    add error handling

--- recommender_api/routes/recommender_route.py
+++ recommender_api/routes/recommender_route.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from ..model.status_queries import get_status_by_id
 
 from recommender_api.services.ranking_system import RankingSystem
 from recommender_api.services.hashtag_modelling import TagGenerator
 from recommender_api.services.nlp_model_loader import NLPModelLoader
+from recommender_api.dto.user_input_dto import UserInputDTO
 
 recommender_route = Blueprint("recommender_route", __name__)
 
@@ -13,22 +14,24 @@ nlp_model_loader.load_model("de_core_news_lg")
 
 ranking_system = RankingSystem()
 
-
 @recommender_route.route("/accounts/<account_id>/create-sorted-timeline", methods=["POST"])
 def sort_timeline(account_id):
     user_input = request.get_json()
-    recommendations = ranking_system.sort_timeline(
-        account_id,
-        user_input["status_ids"],
-    )
+    try:
+        dto = UserInputDTO(**user_input)
+    except TypeError:
+        abort(400)
+
+    recommendations = ranking_system.sort_timeline(account_id, dto.status_ids)
+
     return jsonify(recommendations)
-
-
-@recommender_route.route("/statuses/<status_id>/generate-tags", methods=["POST"])
+ 
+@recommender_route.route("/statuses/<status_id>/generate-tags", methods=["GET"])
 def generate_tag_for_status(status_id):
-    status = get_status_by_id(status_id)
-    if status["local"]:
-        tag_generator = TagGenerator(status, nlp_model_loader)
-        status_with_tag = tag_generator.generate_hashtags()
-        return jsonify(status_with_tag)
-    return jsonify(status)
+    try:
+        status = get_status_by_id(status_id)
+    except IndexError:
+        abort(404)
+    tag_generator = TagGenerator(status, nlp_model_loader)
+    status_with_tag = tag_generator.generate_hashtags()
+    return jsonify(status_with_tag)

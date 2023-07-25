35523316c25e99cd8771c9b294c6a04887c554a3
PhilippVogel92 <107690428+PhilippVogel92@users.noreply.github.com>
2023-07-25 13:05:36 +0200

    add a copy of the ranking system with SR

--- recommender_api/main.py
+++ recommender_api/main.py
@@ -3,14 +3,11 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-# from flask_cors import CORS, cross_origin
 from recommender_api.routes.recommender_route import recommender_route
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(recommender_route)
-# cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
 if __name__ == '__main__':
     app.run(host=os.getenv('FLASK_HOST'), port=os.getenv('FLASK_PORT'))

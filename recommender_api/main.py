869fd6ee13b8e6404378101ec4461a826757eec5
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2023-08-01 11:36:52 +0200

    Fix blueprint variable name

--- recommender_api/main.py
+++ recommender_api/main.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-from recommender_api.routes.recommender_route import recommender_route
+from recommender_api.routes.recommender_route import blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(recommender_route)
+app.register_blueprint(blueprint)
 if __name__ == '__main__':
     app.run(host=os.getenv('FLASK_HOST'), port=os.getenv('FLASK_PORT'))
